750b5a99ec4f9316d59aae0eb91608c76387f801
Stephen Brandon <stephen@sbrandon.com>
2014-11-11 22:09:04 +0000

    Client Update for Test

--- src/com/server/TestClient.java
+++ src/com/server/TestClient.java
@@ -6,16 +6,48 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class TestClient {
+
+	private BufferedReader inFromUser;
+	private DataOutputStream sendMessage;
+	private BufferedReader reader;
+	private int portNumber;
+	private boolean running;
+	
+	public TestClient(int portNumber){
+		inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		this.portNumber = portNumber;
+	}
+	
+	public void start(){
+		System.out.println("Client is running. Port No. " + portNumber);
+		running = true;
+		while(running)
+		{
+			sendCommand();
+		}
+	}
+	
+	public void sendCommand(){
+		try
+		{
+			Socket socket = new Socket("localhost", portNumber);
+			sendMessage = new DataOutputStream(socket.getOutputStream());
+			System.out.println("Enter Command:");
+			sendMessage.writeBytes(inFromUser.readLine() + "\n");
+			reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			System.out.println("FROM SERVER: " + reader.readLine());
+			socket.close();
+		}
+		catch(Exception e)
+		{
+			running = false;
+			System.out.println("Cannot Connect With Server");
+		}
+	}
 	
 	public static void main(String args[]) throws Exception {
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Please Enter a port Number:");
-		int portNumber = Integer.parseInt(inFromUser.readLine());
-		Socket socket = new Socket("localhost", portNumber);
-		System.out.println("Enter Command:");
-		DataOutputStream sendMessage = new DataOutputStream(socket.getOutputStream());
-		sendMessage.writeBytes(inFromUser.readLine() + "\n");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		System.out.println("FROM SERVER: " + reader.readLine());
+		int portNumber = Integer.parseInt(args[0]);
+		TestClient client = new TestClient(portNumber);
+		client.start();
 	}
 }
